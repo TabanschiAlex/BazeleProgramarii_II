7a3c06811c0bdf21868afff6cb3b9e25cc220828
TabanschiAlex <55706654+TabanschiAlex@users.noreply.github.com>
2020-04-23 18:17:18 +0300

    Add files via upload

---
+++ Laboratoare/lab_BP_2-12.pas
@@ -0,0 +1,61 @@
+﻿Type
+  Date = record
+    d:1..31;
+    m:1..12;
+    y:word;
+  end;
+  Passengers = record
+    name: string;
+    distanation: string;
+    departureDate: Date;
+  end;
+  Stack = ^Node;
+  Node = record
+    Info: Passengers;
+    Next: Stack;
+  end;
+Var
+  ListOfPassengers, tmp: Stack;
+  key:integer;
+  datePoint: Date;
+Begin
+  
+  // Создание списка пассажиров
+  
+  ListOfPassengers := nil;
+  key := 1;
+  While ( key = 1 ) do
+    begin
+      new(tmp);
+      Writeln('Добавьте данные пассажира:');
+      Writeln('Введите имя пассажира: ');
+      Readln(tmp^.Info.name);
+      Writeln('Направление(Страна): ');
+      Readln(tmp^.Info.distanation);
+      Writeln('Дата отправления: ');
+      Readln(tmp^.Info.departureDate.d, tmp^.Info.departureDate.m, tmp^.Info.departureDate.y);
+      tmp^.Next := ListOfPassengers;
+      ListOfPassengers := tmp;
+      Write('Есть еще пассажиры?: ');
+      Readln(key);
+    end;
+  
+  // Какие пассажиры остались после n даты вылета
+  
+  Write('Введите дату вылета:' );
+  Readln(datePoint.d, datePoint.m, datePoint.y);
+  tmp := ListOfPassengers;
+  Writeln('Имена пассажиров, которые остались после вылета самолета ', datePoint.d,'.', datePoint.m,'.', datePoint.y );
+  While ( tmp <> nil ) do
+    begin
+      if (tmp^.Info.departureDate.d > datePoint.d) and (tmp^.Info.departureDate.m >= datePoint.m) and (tmp^.Info.departureDate.y >= datePoint.y) then
+        Writeln(tmp^.Info.name)
+      else
+        if (tmp^.Info.departureDate.m > datePoint.m) and (tmp^.Info.departureDate.y >= datePoint.y) then
+          Writeln(tmp^.Info.name)
+        else
+          if (tmp^.Info.departureDate.y > datePoint.y) then
+            Writeln(tmp^.Info.name);
+      tmp := tmp^.Next;
+    end;
+End.
---
+++ Laboratoare/lab_BP_2-7.pas
@@ -0,0 +1,62 @@
+﻿Type
+   Coord = record
+    x: real;
+    y: real;
+    length: real;
+  end;
+  Stack = ^Node;
+  Node = record
+    Info: Coord;
+    Next: Stack;
+  end;
+Var
+  coordList, tmp: Stack;
+  key:integer;
+  maxLength: real;
+Begin
+  
+  // Создание списка координат
+  
+  coordList := nil;
+  key := 1;
+  While ( key = 1 ) do
+    begin
+      new(tmp);
+      Writeln('Добавьте данные координат:');
+      Writeln('Введите значение X: ');
+      Readln(tmp^.Info.x);
+      Writeln('Введите значение Y: ');
+      Readln(tmp^.Info.y);
+      
+      // Вычисление длины отрезков (Правильнее было бы сделать вычесление через теорему Пифагора)
+      
+      if (tmp^.Info.x < 0) and (tmp^.Info.y < 0) then
+        tmp^.Info.length := (tmp^.Info.x*(-1)) + (tmp^.Info.y*(-1))
+      else
+        if (tmp^.Info.y < 0) then
+          tmp^.Info.length := tmp^.Info.x + tmp^.Info.y*(-1)
+        else
+          if (tmp^.Info.x < 0) then
+            tmp^.Info.length := tmp^.Info.x*(-1) + tmp^.Info.y
+          else
+            tmp^.Info.length := tmp^.Info.x + tmp^.Info.y;
+            
+      tmp^.Next := coordList;
+      coordList := tmp;
+      Write('Есть еще координаты?: ');
+      Readln(key);
+    end;
+    
+    // Поиск максимальной длины у координат
+    
+    tmp := coordList;
+    While ( tmp^.Next <> nil ) do
+      begin
+        if (tmp^.Info.length < tmp^.Next^.Info.length) then
+          maxLength := tmp^.Next^.Info.length
+        else
+          maxLength := tmp^.Info.length;
+        tmp := tmp^.Next;
+      end;
+    Write('Максимальный отрезок = ', maxLength)
+End.
